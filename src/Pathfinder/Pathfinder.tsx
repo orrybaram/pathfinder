5c93c035a2292447ce14b0339757e9a2a6e7058e
orrybaram <orrybaram@gmail.com>
2019-10-07 00:15:35 -0400

    feat(App): generate initial grid

--- src/Pathfinder/Pathfinder.tsx
+++ src/Pathfinder/Pathfinder.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Node from './components/Node';
+import './Pathfinder.styles.css';
+
+const GRID_WIDTH = 60;
+const GRID_HEIGHT = 30;
 
 const Pathfinder = () => {
-  return <div>hi</div>;
+  return (
+    <div className="grid">
+      {[...Array(GRID_HEIGHT)].map((_, i) => (
+        <div className="row">
+          {[...Array(GRID_WIDTH)].map((_, j) => {
+            return <Node column={j} row={i} />;
+          })}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Pathfinder;
