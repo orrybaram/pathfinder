a5cff75ccab7a259c6792bee8e3ae260f6111e73
orrybaram <orrybaram@gmail.com>
2019-10-06 23:35:33 -0400

    feat(App): add typescript and strip out unnecessary things

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pathfinder from './Pathfinder';
+
+function App() {
+  return (
+    <div className="App">
+      <Pathfinder />
+    </div>
+  );
+}
+
+export default App;
